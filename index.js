027b828952c6af377d8a7bda79d7de1c580fe9c3
songareravi <ravi.songare@outlook.com>
2025-01-24 20:46:19 +0530

    Connected to atlas DB

--- index.js
+++ index.js
@@ -7,10 +7,42 @@ import path from 'path';
 main().catch(err => console.log(err));
 
 async function main() {
-  await connect('mongodb://127.0.0.1:27017/realEsateDb');
+  // await connect('mongodb://127.0.0.1:27017/realEsateDb');
+  await connect('mongodb+srv://adityanair954:<Hunchman@1234>@real-estate-manager.8uqgb.mongodb.net/?retryWrites=true&w=majority&appName=real-estate-manager');
   console.log('Connected to db')
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://adityanair954:<Hunchman@1234>@real-estate-manager.8uqgb.mongodb.net/?retryWrites=true&w=majority&appName=real-estate-manager";
+ 
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+ 
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
 
 
 const app = express();
